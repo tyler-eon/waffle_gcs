f530cd62ff3001375aeaca166d7f6c4aa6c198c3
Donald A. Lobo <donald.lobo@gmail.com>
2020-10-03 17:15:32 -0700

    get token scope working, update README to fix errors

--- lib/waffle/storage/google/cloud_storage.ex
+++ lib/waffle/storage/google/cloud_storage.ex
@@ -28,7 +28,7 @@ defmodule Waffle.Storage.Google.CloudStorage do
   Put a Waffle file in a Google Cloud Storage bucket.
   """
   @spec put(Types.definition, Types.version, Types.meta) :: object_or_error
-  def put(definition, version, meta) do
+  def put(definition, version, {_file, scope} = meta) do
     path = path_for(definition, version, meta)
     acl = definition.acl(version, meta)
 
@@ -44,16 +44,16 @@ defmodule Waffle.Storage.Google.CloudStorage do
       |> get_gcs_optional_params(version, meta)
       |> ensure_keyword_list()
 
-    insert(conn(), bucket(definition), path, data(meta), gcs_options, gcs_optional_params)
+    insert(conn(scope), bucket(definition), path, data(meta), gcs_options, gcs_optional_params)
   end
 
   @doc """
   Delete a file from a Google Cloud Storage bucket.
   """
-  @spec put(Types.definition, Types.version, Types.meta) :: object_or_error
-  def delete(definition, version, meta) do
+  @spec delete(Types.definition, Types.version, Types.meta) :: object_or_error
+  def delete(definition, version, {_file, scope} = meta) do
     Objects.storage_objects_delete(
-      conn(),
+      conn(scope),
       bucket(definition),
       path_for(definition, version, meta) |> URI.encode_www_form()
     )
